67dc8faff5e2b211e82f350d59f81ba201485854
Maxx <maxxyx96@gmail.com>
2019-08-23 15:06:20 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,9 +3,11 @@ import java.util.*; //For Input
 public class Duke {
     public static void main(String[] args) {
 
+        //list generation
+        ArrayList<String> taskList = new ArrayList<String>();
+
         //input thing
         Scanner newInput = new Scanner(System.in);
-        //TYPE EVERYTHING IN HERE >:( 
 
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -22,11 +24,27 @@ public class Duke {
         String userText = "";
 
         while (!userText.equals("bye")) {
-            userText = newInput.nextLine(); //Should read next line for sentences
-            System.out.println(userText);
+            userText = newInput.nextLine();
+
+            //If list requested
+            if (userText.equals("list")) {
+                //Print List
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.println( (i + 1) + ". " + taskList.get(i));
+                }
+            }
+
+            //Adding into the list
+            else if (!userText.equals("bye")) {
+                taskList.add(userText);
+                System.out.println("added :" + userText);
+            }
+
+            else {
+                System.out.println("Somehow the code ended here...");
+            }
+
         }
-
-
 
         //Bye
         System.out.println("Bye. Hope to see you again soon!");

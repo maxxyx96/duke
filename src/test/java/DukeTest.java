fda73f85f67a69d3d636fffce6ed276070e681ac
maxxyx96 <maxxyx96@gmail.com>
2019-09-12 23:39:02 +0800

    Added more tests for JUnit

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -1,10 +1,19 @@
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DukeTest {
     @Test
     public void dummyTest(){
-        assertEquals(2, 2);
+        assertEquals(2,2);
     }
+
+    @Test
+    public void todoTest() {
+        assertEquals("[T][✗]Testing Duke todo", new Todo("Testing Duke todo").toString() );
+    }
+    @Test
+    public void eventTest() {
+        assertEquals("[T][✗]Testing Duke event", new Todo("Testing Duke event").toString() );
+    }
+
 }
